ec009e386040cb58e9c496bcd0128a3fa2f0616e
DavidGrunheidt <davidordine98@gmail.com>
2020-05-21 00:21:45 -0300

    removing cors policy

--- server.js
+++ server.js
@@ -2,15 +2,10 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
 
 const https = require('https');
 
 const rss_feed = "https://feeds.soundcloud.com/users/soundcloud:users:248666363/sounds.rss"
-
-// app.use(cors({
-//   origin: 'https://rssfeeddoidao.herokuapp.com/'
-// }));
 
 app.get('/api/rssfeed.rss', (req, res) => {
 
@@ -22,8 +17,6 @@ app.get('/api/rssfeed.rss', (req, res) => {
     });
 
     resp.on('end', () => {
-      console.log(typeof data)
-
       res.set('Content-Type', "application/rss+xml;charset=utf-8")
       res.send(data);
     });
